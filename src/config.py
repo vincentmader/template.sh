15ff90d9441e7b2be61d1353336828eb215e7f9b
Vincent C. Mader <vincent.mader@icloud.com>
2023-06-24 13:02:19 +0200

    added template for python flask web server

--- src/config.py
+++ src/config.py
@@ -5,4 +5,5 @@ CLI_ARGS = sys.argv
 
 TEMPLATES = {
     "py": "https://github.com/vincentmader/template_python-project",
+    "flask": "https://github.com/vincentmader/template_python-flask-server",
 }
