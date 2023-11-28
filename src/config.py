7446c9e99380702fcbd3cf4aa150932e69ee2592
Vincent C. Mader <vincent.mader@icloud.com>
2023-11-29 00:04:02 +0100

    FIX repo link
    I updated the repo URL to be consistent with the others

--- src/config.py
+++ src/config.py
@@ -6,5 +6,5 @@ CLI_ARGS = sys.argv
 TEMPLATES = {
     "py": "https://github.com/vincentmader/template_python-project",
     "flask": "https://github.com/vincentmader/template_python-flask-server",
-    "rocket": "https://github.com/vincentmader/project-template_rust-rocket-server",
+    "rocket": "https://github.com/vincentmader/template_rust-rocket-server",
 }
