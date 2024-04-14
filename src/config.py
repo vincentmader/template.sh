f735503bbf6ed2114a6e8e8329cce966f6517e30
Vincent C. Mader <vincent.mader@icloud.com>
2024-04-14 18:50:24 +0200

    FEAT Add template for `yew.rs` client

--- src/config.py
+++ src/config.py
@@ -4,7 +4,8 @@ import sys
 CLI_ARGS = sys.argv
 
 TEMPLATES = {
-    "py": "https://github.com/vincentmader/template_python-project",
-    "flask": "https://github.com/vincentmader/template_python-flask-server",
-    "rocket": "https://github.com/vincentmader/template_rust-rocket-server",
+    "py":       "https://github.com/vincentmader/template_python-project",
+    "flask":    "https://github.com/vincentmader/template_python-flask-server",
+    "rocket":   "https://github.com/vincentmader/template_rust-rocket-server",
+    "yew":      "https://github.com/vincentmader/template_rust-yew-client",
 }
