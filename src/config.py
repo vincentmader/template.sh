0dc1555c241e54bcb46c82b936f4938b732d6aeb
Vincent C. Mader <vincent.mader@icloud.com>
2024-05-09 00:29:27 +0200

    FEAT Add template for static file-server with crate `rocket`

--- src/config.py
+++ src/config.py
@@ -8,4 +8,5 @@ TEMPLATES = {
     "flask":    "https://github.com/vincentmader/template_python-flask-server",
     "rocket":   "https://github.com/vincentmader/template_rust-rocket-server",
     "yew":      "https://github.com/vincentmader/template_rust-yew-client",
+    "fs":       "https://github.com/vincentmader/template_rust-rocket-file-server",
 }
