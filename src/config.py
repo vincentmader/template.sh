ba4f280bbb1d47a12aa51f8b2293cf24ee14d357
Vincent C. Mader <vincent.mader@icloud.com>
2024-06-09 00:49:35 +0200

    FEAT Add templates for binary & library Cargo crates

--- src/config.py
+++ src/config.py
@@ -4,9 +4,11 @@ import sys
 CLI_ARGS = sys.argv
 
 TEMPLATES = {
-    "py":       "https://github.com/vincentmader/template_python-project",
-    "flask":    "https://github.com/vincentmader/template_python-flask-server",
-    "rocket":   "https://github.com/vincentmader/template_rust-rocket-server",
-    "yew":      "https://github.com/vincentmader/template_rust-yew-client",
-    "fs":       "https://github.com/vincentmader/template_rust-rocket-file-server",
+    "cargo-bin": "https://github.com/vincentmader/template_cargo-bin-project",
+    "cargo-lib": "https://github.com/vincentmader/template_cargo-lib-project",
+    "py":        "https://github.com/vincentmader/template_python-project",
+    "py-flask":  "https://github.com/vincentmader/template_python-flask-server",
+    "rocket":    "https://github.com/vincentmader/template_rust-rocket-server",
+    "rocket-fs": "https://github.com/vincentmader/template_rust-rocket-file-server",
+    "yew":       "https://github.com/vincentmader/template_rust-yew-client",
 }
